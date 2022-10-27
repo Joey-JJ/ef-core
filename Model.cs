9365facc6ef6f9d27252431ec18cfe3a8f109061
Joey van der Valk <joeyvandervalk@gmail.com>
2022-10-27 15:36:54 +0200

    Testing the framework

--- Model.cs
+++ Model.cs
@@ -1,39 +1,24 @@
 using Microsoft.EntityFrameworkCore;
 
-public class BloggingContext : DbContext
+public class UserContext : DbContext
 {
-    public DbSet<Blog>? Blogs { get; set; }
-    public DbSet<Post>? Posts { get; set; }
-
+    public DbSet<User>? Users { get; set; }
     public string DbPath { get; }
 
-    public BloggingContext()
+
+    public UserContext()
     {
         var folder = Environment.SpecialFolder.LocalApplicationData;
         var path = Environment.GetFolderPath(folder);
-        DbPath = System.IO.Path.Join(path, "blogging.db");
+        DbPath = System.IO.Path.Join(path, "users.db");
     }
 
-    // The following configures EF to create a Sqlite database file in the
-    // special "local" folder for your platform.
     protected override void OnConfiguring(DbContextOptionsBuilder options)
-        => options.UseSqlite($"Data Source={DbPath}");
+    => options.UseSqlite($"Data Source={DbPath}");
 }
 
-public class Blog
+public class User
 {
-    public int BlogId { get; set; }
-    public string? Url { get; set; }
-
-    public List<Post>? Posts { get; } = new();
-}
-
-public class Post
-{
-    public int PostId { get; set; }
-    public string? Title { get; set; }
-    public string? Content { get; set; }
-
-    public int BlogId { get; set; }
-    public Blog? Blog { get; set; }
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
 }
--- Program.cs
+++ Program.cs
@@ -1,27 +1,19 @@
-﻿using var db = new BloggingContext();
+﻿using var db = new UserContext();
 
-// Note: This sample requires the database to be created before running.
-Console.WriteLine($"Database path: {db.DbPath}.");
 
-// Create
-Console.WriteLine("Inserting a new blog");
-db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
-db.SaveChanges();
+var users =
+    from user in db.Users
+    orderby user.Id
+    select user;
 
-// Read
-Console.WriteLine("Querying for a blog");
-var blog = db.Blogs?
-    .OrderBy(b => b.BlogId)
-    .First();
+// db.AddRange(new User[] {
+//     new User() { Name = "John"},
+//     new User() { Name = "Franco"},
+//     new User() { Name = "Boris"},
+//     new User() { Name = "Henk"},
+// });
 
-// Update
-Console.WriteLine("Updating the blog and adding a post");
-blog!.Url = "https://devblogs.microsoft.com/dotnet";
-blog?.Posts?.Add(
-    new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" });
-db.SaveChanges();
+// db.SaveChanges();
 
-// Delete
-Console.WriteLine("Delete the blog");
-db?.Remove(blog!);
-db!.SaveChanges();
+foreach (var user in users)
+    Console.WriteLine($"Name: {user.Name}, ID: {user.Id}");
